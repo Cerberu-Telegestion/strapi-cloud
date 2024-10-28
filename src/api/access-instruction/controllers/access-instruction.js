3f4602d08dc8748831f7e60087b181fdc81d6dfc
UX <ux@gaiarooms.com>
2024-10-28 14:03:58 +0100

    adding hotel access info

---
+++ src/api/access-instruction/controllers/access-instruction.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * access-instruction controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::access-instruction.access-instruction');
---
+++ src/api/access-instruction/routes/access-instruction.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * access-instruction router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::access-instruction.access-instruction');
