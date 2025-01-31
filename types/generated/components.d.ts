a1fb1168a4829450255fcdfebe24e9544da5ef12
UX <ux@gaiarooms.com>
2025-01-31 09:32:21 +0100

    Removing unused componets

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -15,39 +15,6 @@ export interface SharedLocationFeature extends Schema.Component {
   };
 }
 
-export interface SharedLongStaysLocationFeature extends Schema.Component {
-  collectionName: 'components_shared_long_stays_location_features';
-  info: {
-    displayName: 'LongStaysLocationFeature';
-  };
-  attributes: {
-    gym: Attribute.Boolean & Attribute.DefaultTo<false>;
-    coWorking: Attribute.Boolean & Attribute.DefaultTo<false>;
-    communitySaloon: Attribute.Boolean & Attribute.DefaultTo<false>;
-    pool: Attribute.Boolean & Attribute.DefaultTo<false>;
-    disabilityAdaptation: Attribute.Boolean & Attribute.DefaultTo<false>;
-    multipurposeRoom: Attribute.Boolean & Attribute.DefaultTo<false>;
-    garden: Attribute.Boolean & Attribute.DefaultTo<false>;
-    rooftop: Attribute.Boolean & Attribute.DefaultTo<false>;
-    communityTerrace: Attribute.Boolean & Attribute.DefaultTo<false>;
-    smartLockers: Attribute.Boolean & Attribute.DefaultTo<false>;
-    digitalConcierge: Attribute.Boolean & Attribute.DefaultTo<false>;
-  };
-}
-
-export interface SharedLongStaysZoneFeatures extends Schema.Component {
-  collectionName: 'components_shared_long_stays_zone_features';
-  info: {
-    displayName: 'LongStaysZoneFeatures';
-  };
-  attributes: {
-    privateBathroom: Attribute.Boolean & Attribute.DefaultTo<true>;
-    ovenMicrowave: Attribute.Boolean & Attribute.DefaultTo<true>;
-    kitchen: Attribute.Boolean & Attribute.DefaultTo<true>;
-    privateGarden: Attribute.Boolean & Attribute.DefaultTo<false>;
-  };
-}
-
 export interface SharedMedia extends Schema.Component {
   collectionName: 'components_shared_media';
   info: {
@@ -75,8 +42,6 @@ declare module '@strapi/types' {
   export module Shared {
     export interface Components {
       'shared.location-feature': SharedLocationFeature;
-      'shared.long-stays-location-feature': SharedLongStaysLocationFeature;
-      'shared.long-stays-zone-features': SharedLongStaysZoneFeatures;
       'shared.media': SharedMedia;
       'shared.rich-text': SharedRichText;
     }
