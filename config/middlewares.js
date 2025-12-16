9f1234f4d637503c6847bea7cfa3fc785a256022
UX <ux@gaiarooms.com>
2025-12-16 19:18:13 +0100

    Adding global::cache-headers

--- config/middlewares.js
+++ config/middlewares.js
@@ -8,13 +8,11 @@ module.exports = [
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
+  'strapi::public',
   {
-    name: 'strapi::public',
+    name: 'global::cache-headers',
     config: {
-      maxAge: 31536000,   // 1 year
-      immutable: true,
-      cacheControl: 'public',
-      path: './public', // default
+      enabled: true,
     },
   },
 ];
